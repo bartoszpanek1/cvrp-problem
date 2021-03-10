eb51e5cdef1dc80270d0dfe295d701820c9009c4
b-panek <b.panek@samsung.com>
2021-03-10 19:09:16 +0100

    Added graph classes

---
+++ MSI2/algorithms/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MSI2.graph;
+
+namespace MSI2.algorithms
+{
+    interface CVRPSolver
+    {
+        (List<List<Node>> VisitedNodes, int TotalDistance) solve(Graph graph);
+    }
+}
---
+++ MSI2/graph/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MSI2.graph
+{
+    class Node
+    {
+        public int id, demand;
+        public bool visited;
+        Node(int id, int demand)
+        {
+            this.id = id;
+            this.demand = demand;
+            visited = false;
+        }
+        public override bool Equals(object obj) => id == ((Node)obj).id;
+    }
+    class Graph
+    {
+        Dictionary<Node, List<(Node node, int distance)>> AdjList { get; set; }
+
+        void AddNode(Node node)
+        {
+            if (!AdjList.ContainsKey(node))
+            {
+                AdjList.Add(node, new List<(Node, int)>());
+            }
+        }
+
+        void AddEdge(Node from, Node to, int distance)
+        {
+            if (!AdjList[from].Any(n => n.node == to))
+            {
+                AdjList[from].Add((to, distance));
+            }
+        }
+    }
+}

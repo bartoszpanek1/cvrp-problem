e337f49129f135045fb31ce1744064c1852f248f
b-panek <b.panek@samsung.com>
2021-03-10 23:06:59 +0100

    Added first version of greedy solver

--- MSI2/Algorithms/GreedySolver.cs
+++ MSI2/Algorithms/GreedySolver.cs
@@ -1,6 +1,7 @@
 ﻿using MSI2.Graphs;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MSI2.Algorithms
 {
@@ -8,7 +9,53 @@ namespace MSI2.Algorithms
     {
         public (List<List<Node>> VisitedNodes, int TotalDistance) Solve(Graph graph, int vehiclesNumber, int capacity, int sMax)
         {
-            throw new NotImplementedException();
+
+            int visitedNodesNum = 0;
+            int totalDistance = 0;
+            var visitedNodes = new List<List<Node>>();
+            for (int i = 0; i < vehiclesNumber; i++)
+            {
+                Node lastNode = graph.NodeList[0]; ;
+                Node currentNode = graph.NodeList[0];
+                int currentDistance = sMax;
+                int currentCapacity = capacity;
+                var visited = new List<Node>();
+                while (currentNode != null)
+                {
+                    visitedNodesNum++;
+                    visited.Add(currentNode);
+                    currentDistance -= graph.GetDistance(lastNode.id, currentNode.id);
+                    totalDistance += graph.GetDistance(lastNode.id, currentNode.id);
+
+                    currentCapacity -= currentNode.demand;
+                    lastNode = currentNode;
+                    currentNode = NextNode(graph, currentDistance, currentNode.id, currentCapacity);
+                    if (currentNode.id == 0)
+                    {
+                        visitedNodes.Add(visited);
+                        break;
+                    }
+                    currentNode.visited = true;
+                }
+
+                if (visitedNodesNum == graph.NodeList.Count)
+                {
+                    return (visitedNodes, totalDistance);
+                }
+            }
+            return (null, -1);
+        }
+
+        public Node NextNode(Graph graph, int currentDistance, int currentNodeNumber, int currentCapacity)
+        {
+            var sortedByValidCapacity = graph.AdjList[currentNodeNumber]
+                .Where(n => currentCapacity - graph.NodeList[n.id].demand >= 0 && !graph.NodeList[n.id].visited)
+                .OrderBy(n => n.distance).ToList();
+            if (sortedByValidCapacity.Any() && sortedByValidCapacity[0].distance <= currentDistance)
+            {
+                return graph.NodeList[sortedByValidCapacity[0].id];
+            }
+            return null;
         }
     }
 }
--- MSI2/Graphs/Graph.cs
+++ MSI2/Graphs/Graph.cs
@@ -5,22 +5,33 @@ namespace MSI2.Graphs
 {
     public class Graph
     {
-        Dictionary<Node, List<(Node node, int distance)>> AdjList { get; set; }
+        public Dictionary<int, List<(int id, int distance)>> AdjList { get; set; }
+        public List<Node> NodeList { get; set; }
 
         public void AddNode(Node node)
         {
-            if (!AdjList.ContainsKey(node))
+            if (!AdjList.ContainsKey(node.id))
             {
-                AdjList.Add(node, new List<(Node, int)>());
+                AdjList.Add(node.id, new List<(int, int)>());
+                NodeList.Add(node);
             }
         }
 
-        public void AddEdge(Node from, Node to, int distance)
+        public void AddEdge(int from, int to, int distance)
         {
-            if (!AdjList[from].Any(n => n.node == to))
+            if (!AdjList[from].Any(n => n.id == to))
             {
                 AdjList[from].Add((to, distance));
             }
+        }
+
+        public int GetDistance(int from, int to)
+        {
+            if(from == to)
+            {
+                return 0;
+            }
+            return AdjList[from].First(n => n.id == to).distance;
         }
     }
 }

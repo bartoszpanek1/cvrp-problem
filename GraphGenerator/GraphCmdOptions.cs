c6bc91bd8d87417b7d1ab5192af3147b1c9ff954
Piotr Kośkiewicz <piotr888k@gmail.com>
2021-03-24 20:49:13 +0100

    Add graph generator project, not working yet

---
+++ GraphGenerator/GraphCmdOptions.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphGenerator
+{
+    public class GraphCmdOptions
+    {
+
+        [Option('o', "output", Required = true, HelpText = "Output file")]
+        public string OutputFile { get; set; }
+
+        [Option('v', "vertives", Required = true, HelpText = "Number of vertices")]
+        public int VerticesNumber { get; set; }
+
+        [Option('d', "demand", Required = true, HelpText = "Max demand")]
+        public int MaxDemand { get; set; }
+
+        [Option('w', "weight", Required = true, HelpText = "Max edge weight")]
+        public int MaxWeight { get; set; }
+    }
+}
---
+++ GraphGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using CommandLine;
+using System;
+
+namespace GraphGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadArgs(
+                args,
+                out string outputPath,
+                out int verticesNumber,
+                out int maxDemand,
+                out int maxWeight);
+
+
+        }
+
+        private static void ReadArgs(
+            string[] args,
+            out string outputPath,
+            out int verticesNumber,
+            out int maxDemand,
+            out int maxWeight)
+        {
+            string output = string.Empty;
+            int v = 0, d = 0, w = 0;
+
+            Parser.Default.ParseArguments<GraphCmdOptions>(args)
+                   .WithParsed(o =>
+                   {
+                       output = o.OutputFile;
+                       v = o.VerticesNumber;
+                       d = o.MaxDemand;
+                       w = o.MaxWeight;
+                   });
+
+            outputPath = output;
+            verticesNumber = v;
+            maxDemand = d;
+            maxWeight = w;
+        }
+    }
+}
